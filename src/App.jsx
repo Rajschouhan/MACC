81e4d949d22e87a59f508b9f467a33142e7addd8
Rajschouhan <rajsinghchouhan54967@gmail.com>
2025-11-16 22:07:30 +0530

    Initial Vite + Bootstrap setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+     <h1 className="container text-center mt-5">MACC Test - Bootstrap Works!</h1>
+      
+    </>
+  )
+}
+
+export default App
